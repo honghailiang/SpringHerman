e29f3ae16b817cc4cfc2d2f0da91348e95a6e33d
honghailiang <honghailaing666@126.com>
2018-02-26 11:19:56 +0800

    swagger优化

--- src/main/java/com/herman/springherman/controller/SpringHermanController.java
+++ src/main/java/com/herman/springherman/controller/SpringHermanController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api("user相关api")
+@Api(description = "user相关api")
 @RestController
 @RequestMapping("/springherman")
 public class SpringHermanController {
